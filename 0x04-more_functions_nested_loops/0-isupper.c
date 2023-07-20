fd44775e67c78f1cc8ebdbb9cea215f4474e1bad
abidec44 <abideenakin@yahoo.com>
2023-07-20 16:25:11 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-isupper.c -o 0-isuper\\\\\\\\\\\\\\

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * _isupper - Checks for lowercase character
+ * @c: The character to be checked
+ *
+ * Return: 1 for uppercase character or 0 for anything else
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c)
+
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
