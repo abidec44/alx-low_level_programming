a49583d6c92467e2bc445e7e304c1a97e9f650b4
abidec44 <abideenakin@yahoo.com>
2023-07-27 07:15:16 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
-
 /**
- * _strlen - returns the length of a string
+ * _strlen - retuns the length of a string
  *
- * @s: input parameter 1
+ * @s: input prameter 1
  *
- * Return: the length of the string
+ * Return: legth of string
  */
+
 int _strlen(char *s)
 {
-
 	int counter;
 
 	for (counter = 0; *s != '\0'; s++)
 		++counter;
-
 	return (counter);
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,14 +1,13 @@
-#include <unistd.h>
-
 /**
- * _putchar.c - writes the character c tp stdout
- * @c: The character to print
+ * _putchar - writers the character c stdout
+ * @c: the character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return : On success 1.
+ * On error -1 is returned. and erro is set apporprti
+ * ately.
  */
-int _putchar(char c).
+
+int _putchar(char c)
 {
         return (write(1, &c, 1));
-        
-}     
+} 
