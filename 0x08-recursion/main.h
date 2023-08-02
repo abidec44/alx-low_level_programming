51700f5363661e3422cf97fb6aef5e00c3a8edf3
abidec44 <abideenakin@yahoo.com>
2023-08-02 15:01:39 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 void _puts_recursion(char *s);
-int _putchar.c(char c);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -10,5 +10,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-
 #endif
