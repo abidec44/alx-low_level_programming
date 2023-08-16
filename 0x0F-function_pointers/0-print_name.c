2334886688920228437e4d629ed63e946905e7ff
abidec44 <abideenakin@yahoo.com>
2023-08-16 05:55:08 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#Include "function_pointers.h
+#Include <stdio.h>
+/**
+ * print_name - print name using pointer to function
+ * @name:  string to add
+ * @f:  pointer to function
+ * Return:  nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
